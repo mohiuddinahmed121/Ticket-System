7c319d396084a46cc200978bfb05464d1b5a7835
mohiuddinahmed121 <mohiuddinahmed121@gmail.com>
2026-03-05 15:02:23 +0600

    Banner design added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Baner from "./Component/Banner/Baner";
 import Navbar from "./Component/Navbar/Navbar";
 
 function App() {
    return (
       <>
          <Navbar></Navbar>
+         <Baner></Baner>
       </>
    );
 }
---
+++ src/Component/Banner/Baner.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Baner = () => {
+   return (
+      <div className=" bg-gray-300">
+         <div className="max-w-[1200px] mx-auto flex justify-between items-center text-white py-20 gap-7">
+            <div className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] w-[600px] h-[200px] rounded-xl text-center">
+               <h1 className="text-2xl mt-12">In-Progress</h1>
+               <h1 className="text-4xl font-bold mt-2">{0}</h1>
+            </div>
+            <div className="bg-[#422AD5] w-[600px] h-[200px] rounded-xl text-center">
+               <h1 className="text-2xl mt-12">Resolved</h1>
+               <h1 className="text-4xl font-bold mt-2">{0}</h1>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default Baner;
