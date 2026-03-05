c86a62a273f1ee0a611259b90d982d88d9a37968
mohiuddinahmed121 <mohiuddinahmed121@gmail.com>
2026-03-05 14:27:58 +0600

    navbar added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./Component/Navbar/Navbar";
+
+function App() {
+   return (
+      <>
+         <Navbar></Navbar>
+      </>
+   );
+}
+
+export default App;
